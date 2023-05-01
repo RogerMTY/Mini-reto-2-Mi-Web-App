e1699d7ee2cb3567ab1e33be4cf7acda191bb7f6
YahirRivera04 <95262988+YahirRivera04@users.noreply.github.com>
2023-04-30 22:56:22 -0600

    Add files via upload

---
+++ DataBase/DB_MiniReto2.sql
@@ -0,0 +1,13 @@
+use pokemon;
+
+create table iniciales(
+nombre varchar(30),
+region varchar(30),
+numeroPokedex int(5),
+tipo varchar(50),
+debilidad varchar(50),
+imagen varchar(150)
+
+);
+select * from iniciales;
+drop table iniciales;
